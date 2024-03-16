4d61ffc65d7f97a3de8b64446b84f068a019bdc9
Caetano <122699973+ProgCaetano@users.noreply.github.com>
2024-03-16 10:16:41 -0300

    botão after before slide

---
+++ slideShow.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const images = [
+    {'id': '1', 'url':'./img/star1.jpg'},
+    {'id': '2', 'url':'./img/star2.jpg'},
+    {'id': '3', 'url':'./img/star3.jpg'},
+    {'id': '4', 'url':'./img/star4.jpg'},
+]
+
+
+const container = document.querySelector('#container-item')
+
+const loadImages = (images, container) => {
+    images.forEach(image => {
+        container.innerHTML  += `
+            <div class ="item">
+                <img src='${image.url}'
+            </div>
+        ` 
+    });
+}
+
+
+// loadImages(images, container);
