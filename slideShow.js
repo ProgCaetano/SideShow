9f2e6da86f1ec447b85ccc9aa6c5f4822a53211b
Caetano <122699973+ProgCaetano@users.noreply.github.com>
2024-03-16 12:40:34 -0300

    funcçoes
    Adicionado botões after before e transição animada das imagens.

--- slideShow.js
+++ slideShow.js
@@ -8,7 +8,7 @@ const images = [
 ]
 
 
-const container = document.querySelector('#container-item')
+const container = document.querySelector('#container-item');
 
 const loadImages = (images, container) => {
     images.forEach(image => {
@@ -20,5 +20,20 @@ const loadImages = (images, container) => {
     });
 }
 
-
 loadImages(images, container);
+
+let itens = document.querySelectorAll('.item');
+
+const previous = () => {
+    container.appendChild(itens[0]);
+    itens = document.querySelectorAll('.item');
+}
+
+const next = () => {
+    const lastItem = itens[itens.length - 1];
+    container.insertBefore(lastItem, itens[0]);
+    itens = document.querySelectorAll('.item');
+}
+
+document.querySelector('#previous').addEventListener('click', previous);
+document.querySelector('#next').addEventListener('click', next);
